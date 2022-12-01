e8d00f0947456d18b73840dbc2c400d037af25de
Sujoy Ghosal <sujoy143656@gmail.com>
2022-11-30 18:58:11 -0800

    my third portfolio

---
+++ java.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector(".navbar-toggler");
+let navbarToggler = document.getElementById("navbarcontent");
+btn.addEventListener("click", () => {
+    document.getElementById("navbarcontent").classList.toggle("toggleadd");
+});
+
+
+
+
